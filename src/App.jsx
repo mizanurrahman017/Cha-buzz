060b9cadf72821c11997ecb45501bf1b988908a6
unknown <mizanasif1@gmail.com>
2026-09-10 01:29:15 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+       
+        <div>
+          <h1>Cha Buzz</h1>
+        </div>
+        
+      </section>
+
+    
+    </>
+  )
+}
+
+export default App

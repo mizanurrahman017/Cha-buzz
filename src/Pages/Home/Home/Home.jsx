6667b0280be45b16529726eab38726132a71f8cc
unknown <mizanasif1@gmail.com>
2026-09-13 00:03:36 +0600

    fix food

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useMemo, useState } from "react";
 import foods from "../../../Data/foods";
 // import foods from "../../../Data/foods";
+// import foods from "../../../Data/foods";
 import CategorySidebar from "../../../Components/Home/CategorySidebar";
 import FoodGrid from "../../../Components/Home/FoodGrid";
 

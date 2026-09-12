d3cf55b8efd28c808809b190d2b867036d8a011c
unknown <mizanasif1@gmail.com>
2026-09-12 23:57:38 +0600

    cmnt

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -3,59 +3,42 @@ import { createBrowserRouter } from "react-router";
 import RootLayouts from "../Layout/RootLayouts";
 
 import Home from "../Pages/Home/Home/Home";
-
 import Cart from "../Pages/Cart/Cart";
 
-// import Orders from "../Pages/Admin/Orders/Orders";
 import Orders from "../Pages/Admin/Order/Orders";
 
 import AdminRoute from "./AdminRoute";
-import Login from "../Pages/Auth/Login/Login";
-import Waiter from "../Pages/Waiter/Waiter";
 import WaiterRoute from "./WaiterRoute";
 
+import Login from "../Pages/Auth/Login/Login";
+import Waiter from "../Pages/Waiter/Waiter";
 
 export const router = createBrowserRouter([
-
   {
     path: "/",
-
     Component: RootLayouts,
 
     children: [
-
       {
         index: true,
         Component: Home,
       },
 
-
       {
         path: "cart",
         Component: Cart,
       },
+
       {
         path: "login",
         Component: Login,
       },
+
+      // =========================
+      // WAITER ROUTE
+      // =========================
       {
         path: "waiter",
-        Component: Waiter,
-      },
-
-
-      {
-        path: "admin/orders",
-
-        element: (
-          <AdminRoute>
-            <Orders />
-          </AdminRoute>
-        ),
-      },
-      {
-        path: "waiter",
-
         element: (
           <WaiterRoute>
             <Waiter />
@@ -63,8 +46,17 @@ export const router = createBrowserRouter([
         ),
       },
 
-
+      // =========================
+      // ADMIN ROUTE
+      // =========================
+      {
+        path: "admin/orders",
+        element: (
+          <AdminRoute>
+            <Orders />
+          </AdminRoute>
+        ),
+      },
     ],
   },
-
 ]);
